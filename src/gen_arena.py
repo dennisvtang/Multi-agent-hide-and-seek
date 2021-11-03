730219dbff2b4f920c05e7425c2179228877034e
dennisvtang <dennisvtang@gmail.com>
2021-11-02 19:43:01 -0700

    Modify observer agent to provide a top down view

--- src/gen_arena.py
+++ src/gen_arena.py
@@ -95,10 +95,10 @@ def gen_mission_xml(arena_size: int):
 
     # setup agent as observer
     mission_string += f"""
-        <AgentSection mode="Survival">
-            <Name>MalmoTutorialBot</Name>
+        <AgentSection mode="Spectator">
+            <Name>TopDownView</Name>
             <AgentStart>
-                <Placement x="0.5" y="20" z="0.5" pitch="90"/>
+                <Placement x="0" y="{10 + (arena_size//3)}" z="0" pitch="90"/>
             </AgentStart>
             <AgentHandlers>
                 <ObservationFromFullStats/>
